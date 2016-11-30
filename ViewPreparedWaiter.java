ac8b1f468fcf6d02aaf7881e0cb9ffe501ce6d07
firemaples <firemaples@gmail.com>
2016-11-30 12:48:14 +0800

    Update ViewPreparedWaiter.java

--- ViewPreparedWaiter.java
+++ ViewPreparedWaiter.java
@@ -89,7 +89,7 @@ public class ViewPreparedWaiter {
         return true;
     }
 
-    interface OnViewPrepared {
+    public interface OnViewPrepared {
         void onViewPrepared(View viewToWait);
     }
 }
